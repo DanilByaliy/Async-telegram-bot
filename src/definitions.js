5dea8fa1d6b56e5e3efa72f21b7c3c8bc5653f3f
holdennekt <ngryshchak@gmail.com>
2022-01-09 02:58:43 +0200

    refactor + prettier

---
+++ src/definitions.js
@@ -0,0 +1,93 @@
+'use strict';
+module.exports = {
+  home: [['Фильм по ключевым словам'], ['Случайный фильм']],
+  genres: [
+    [
+      {
+        text: 'Боевик',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'action',
+        }),
+      },
+      {
+        text: 'Мультфильм',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'animation',
+        }),
+      },
+      {
+        text: 'Комедия',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'comedy',
+        }),
+      },
+    ],
+    [
+      {
+        text: 'Фантастика',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'fantasy',
+        }),
+      },
+      {
+        text: 'Ужасы',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'horror',
+        }),
+      },
+      {
+        text: 'Триллер',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'thriller',
+        }),
+      },
+    ],
+    [
+      {
+        text: 'Драма',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'drama',
+        }),
+      },
+      {
+        text: 'Аниме',
+        callback_data: JSON.stringify({
+          api: 'kinopoisk',
+          genre: 24,
+        }),
+      },
+      {
+        text: 'Детектив',
+        callback_data: JSON.stringify({
+          api: 'imdb',
+          genre: 'crime',
+        }),
+      },
+    ],
+  ],
+  genresID: {
+    11: 'боевик',
+    24: 'аниме',
+    13: 'комедия',
+    6: 'фантастика',
+    17: 'ужасы',
+    1: 'триллер',
+  },
+  imdbPrelink:
+    'https://imdb-api.com/API/AdvancedSearch/k_ulfxd37t?title_type=feature&user_rating=7.0,10&genres=',
+  imdbPostlink: '&groups=top_1000&count=250&sort=user_rating,desc',
+  kinopoiskPrelink:
+    'https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=',
+  kinopoiskPostlink: '&order=RATING&type=ALL&ratingFrom=7&page=',
+  imdbToKinopoiskPrelink:
+    'https://kinopoiskapiunofficial.tech/api/v2.2/films?type=ALL&imdbId=',
+  imdbToKinopoiskPostlink: '&page=1',
+  kinopoiskApiPagesCount: 20,
+};
---
+++ src/funcs.js
@@ -0,0 +1,107 @@
+const request = require('request');
+const defs = require('./definitions');
+
+const loger = (err) => console.log(err);
+
+const memoize = (fn) => {
+  const cache = Object.create(null);
+  return async (...args) => {
+    const key = args[0];
+    const val = cache[key];
+    if (val) return val;
+    const res = await fn(...args);
+    cache[key] = res;
+    return res;
+  };
+};
+
+const makeRequest = (options) =>
+  new Promise((resolve, reject) => {
+    request(null, options, (error, response) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(response.body));
+    });
+  });
+
+const imdbLinkGenerator = (genre) => {
+  const preLink = defs.imdbPrelink;
+  const postLink = defs.imdbPostlink;
+  return preLink + genre + postLink;
+};
+
+const kinopoiskLinkGenerator = (genre, page) => {
+  const preLink = defs.kinopoiskPrelink;
+  const postLink = defs.kinopoiskPostlink;
+  return preLink + genre + postLink + page;
+};
+
+const kinopoiskFromImdbLinkGenerator = (id) => {
+  const preLink = defs.imdbToKinopoiskPrelink;
+  const postLink = defs.imdbToKinopoiskPostlink;
+  return preLink + id + postLink;
+};
+
+const sendFilm = (film, chatId) => {
+  let genreOfFilm = ' ';
+  for (const genre of film.genres) {
+    if (film.genres.indexOf(genre) === film.genres.length - 1) {
+      genreOfFilm += genre.genre;
+    } else {
+      genreOfFilm += genre.genre + ', ';
+    }
+  }
+  const cap =
+    `Название: ${film.nameRu}\nГод выпуска: ${film.year}\n` +
+    `Рейтинг: ${film.ratingImdb}\nЖанры: ${genreOfFilm}`;
+  bot.sendPhoto(chatId, film.posterUrl, { caption: cap });
+};
+
+const options = (genre, page) => ({
+  method: 'GET',
+  url: kinopoiskLinkGenerator(genre, page),
+  headers: {
+    'X-API-KEY': '4763ee9d-133f-4291-ae48-6066a1ba76bb',
+    'Content-Type': 'application/json',
+  },
+});
+
+const getKinopoiskFilms = async (genre) => {
+  const kinopoiskFilmsArr = [];
+  for (let page = 1; page <= defs.kinopoiskApiPagesCount; page++) {
+    const result = await makeRequest(options(genre, page)).catch(loger);
+    kinopoiskFilmsArr.push(...result.items);
+  }
+  console.log({ length: kinopoiskFilmsArr.length });
+  return kinopoiskFilmsArr;
+};
+
+const mGetKinopoiskFilms = memoize(getKinopoiskFilms);
+
+const getImdbFilms = async (genre) => {
+  const options = {
+    method: 'GET',
+    url: imdbLinkGenerator(genre),
+  };
+  const imdbFilmsArr = [];
+  const result = await makeRequest(options).catch(loger);
+  imdbFilmsArr.push(...result.results);
+  console.log({ length: imdbFilmsArr.length });
+  return imdbFilmsArr;
+};
+
+const mGetImdbFilms = memoize(getImdbFilms);
+
+const getKinopoiskFilmFromImdb = async (film) => {
+  const options = {
+    method: 'GET',
+    url: kinopoiskFromImdbLinkGenerator(film.id),
+    headers: {
+      'X-API-KEY': '4763ee9d-133f-4291-ae48-6066a1ba76bb',
+      'Content-Type': 'application/json',
+    },
+  };
+  const res = await makeRequest(options).catch(loger);
+  return res.items[0];
+};
+
+const randomFilm = (films) => films[Math.floor(Math.random() * films.length)];
